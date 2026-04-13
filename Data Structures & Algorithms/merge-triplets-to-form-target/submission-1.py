6ee7472785a7372950f0a2e5692d02cad21b0a07
Utsav Kuchhal <47279585+utsavkuchhal@users.noreply.github.com>
2026-04-13 18:46:55 +0530

    Add: merge-triplets-to-form-target - submission-1

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        n = len(triplets)
+        num1, num2, num3 = target
+        ans1, ans2, ans3 = float('-inf'), float('-inf'), float('-inf')
+        for index in range(n):
+            curr1, curr2, curr3 = triplets[index]
+            if curr1 <= num1 and curr2 <= num2 and curr3 <= num3:
+                ans1 = max(ans1, curr1)
+                ans2 = max(ans2, curr2)
+                ans3 = max(ans3, curr3)
+        
+        return (ans1, ans2, ans3) == tuple(target)
+
+
+            
