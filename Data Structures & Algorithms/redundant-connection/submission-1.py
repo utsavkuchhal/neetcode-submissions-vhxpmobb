8f5f27916dd954eea05a78179e12546b6e22facf
Utsav Kuchhal <47279585+utsavkuchhal@users.noreply.github.com>
2026-05-04 13:33:07 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.py
@@ -0,0 +1,26 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n + 1)]
+        print(self.parent)
+
+    def union(self, u, v):
+        parent_u = self.find(u)
+        parent_v = self.find(v)
+        if parent_u == parent_v:
+            return True
+        else:
+            self.parent[parent_v] = parent_u
+            return False
+
+    def find(self, u):
+        if self.parent[u] == u:
+            return self.parent[u]
+        return self.find(self.parent[u])
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        uf = UnionFind(len(edges))
+        for u, v in edges:
+            if uf.union(u, v):
+                return [u, v]
+        return []
